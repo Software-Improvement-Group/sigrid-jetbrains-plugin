42206b81bf8a100e89b74bf8db0bb7b184094d58
Andrius Tolvaišis <andrius.tolvaisis@softwareimprovementgroup.com>
2026-06-02 19:07:42 +0300

    Remove redundant JUnit 4 dependency from build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,6 @@ tasks.test {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testRuntimeOnly("junit:junit:4.13.2")
-
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
         intellijIdea("2026.1")
